d71a1c25e39e8b930364e4d5b161c9347796f0f1
Francesco Raviglione <francescorav.es483@gmail.com>
2019-04-02 16:48:54 +0200

    Updated version.h of the development branch to show that the current version is a development one

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef LATENCYTEST_VERSION_H_INCLUDED
 #define LATENCYTEST_VERSION_H_INCLUDED
 	
-#define VERSION "0.1.2-beta"
-#define DATE "20190315a"
+#define VERSION "0.1.2-beta-development"
+#define DATE "20190402a"
 #define PROG_NAME_LONG "LaTe utility"
 #define PROG_NAME_SHORT "LaTe"
 #define GITHUB_LINK "https://github.com/francescoraves483/LaMP_LaTe"
