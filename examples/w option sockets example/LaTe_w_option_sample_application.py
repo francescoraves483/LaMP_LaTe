98616fe83b0b45581cf1892273544d7c0e134184
Francesco Raviglione <francescorav.es483@gmail.com>
2020-05-12 15:57:36 +0200

    Removing unnecessary ';' in the Python 3 -w example
    This is done to keep the coding style consistent throughtout
    the whole Phyton 3 example code

--- examples/w option sockets example/LaTe_w_option_sample_application.py
+++ examples/w option sockets example/LaTe_w_option_sample_application.py
@@ -89,112 +89,112 @@ def main (argv):
 
 	# Create a TCP socket as server (point (1) in ./LaTe -h)
 	try:
-		tcp_descriptor=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+		tcp_descriptor=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	except IOError as e:
-		print("Error when opening the TCP socket. Details:",e.strerror);
-		sys.exit(1);
+		print("Error when opening the TCP socket. Details:",e.strerror)
+		sys.exit(1)
 
-	tcp_descriptor.bind((opts.IPv4,opts.port));
+	tcp_descriptor.bind((opts.IPv4,opts.port))
 
 	# We are accepting only one connection at a time
 	# This is not mandatory: it was our choice in this example to accept one connection at a time
 	# (In reality, 2 connections will be probably accepted, as the backlog parameter in listen() 
 	# is not very rigid and it is more intended as an "hint" given to the kernel)
-	tcp_descriptor.listen(1);
+	tcp_descriptor.listen(1)
 
 	# Create a UDP socket
 	try:
-		udp_descriptor=socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+		udp_descriptor=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	except IOError as e:
-		print("Error when opening the UDP socket. Details:",e.strerror);
-		sys.exit(1);
+		print("Error when opening the UDP socket. Details:",e.strerror)
+		sys.exit(1)
 
-	udp_descriptor.bind((opts.IPv4,opts.port));
+	udp_descriptor.bind((opts.IPv4,opts.port))
 
 	# Main TCP server loop
 	while True:
 		# Wait for a LaTe instance (TCP client) to connect (point (1) in ./LaTe -h)
-		print("Waiting for a connection to " + opts.IPv4 + ":" + str(opts.port) + "...");
+		print("Waiting for a connection to " + opts.IPv4 + ":" + str(opts.port) + "...")
 
-		tcp_descriptor_client,address=tcp_descriptor.accept();
+		tcp_descriptor_client,address=tcp_descriptor.accept()
 
 		# Reset 'exitflag' to 0
 		# No mutex is used as there should be no 'udp_rx_loop' running at this point
-		exitflag=0;
+		exitflag=0
 
-		print("New connection from: ",address);
+		print("New connection from: ",address)
 
 		# Wait for the first TCP packet
-		data=tcp_descriptor_client.recv(2048);
+		data=tcp_descriptor_client.recv(2048)
 
 		# If the peer disconnects too early, print an error message and wait for another connection
 		if len(data)==0:
-			print("Error: LaTe was disconnected too early.");
-			continue;
+			print("Error: LaTe was disconnected too early.")
+			continue
 
 		# Convert 'data' from bytes to string
-		decoded_data=data.decode("utf-8").split(",");
+		decoded_data=data.decode("utf-8").split(",")
 
 		# Check if this packet is a 'LaTeINIT' one or if is it already a 'LaTeEND' packet (client-side unidirectional testing)
 		if decoded_data[0] != "LaTeINIT" and decoded_data[0] != "LaTeEND":
 			# No known packet has been received, close the socket and wait for a new connection
-			print("Expected a LaTeINIT or LateEND packet, but received",decoded_data[0]);
-			tcp_descriptor_client.close();
+			print("Expected a LaTeINIT or LateEND packet, but received",decoded_data[0])
+			tcp_descriptor_client.close()
 			continue;
 		elif decoded_data[0] == "LaTeEND" :
 			# If 'LaTeEND' is received without any 'LaTeINIT' preceding it, a unidirectional client is involved and no per-packet
 			# data is expected to be received. In this case, just save all the report data contained inside, processing it
 			# depending on the user's needs (calling, in our case, final_test_data_operations()) (point (b.1) in ./LaTe -h)
-			final_test_data_operations(decoded_data);
-			tcp_descriptor_client.close();
-			continue;
+			final_test_data_operations(decoded_data)
+			tcp_descriptor_client.close()
+			continue
 		else:
-			print("Received a",decoded_data[0],"packet");
+			print("Received a",decoded_data[0],"packet")
 
 		# If 'LateINIT' is received, save which per-packet fields will be sent via UDP and the current test ID (point (a.1) ./LaTe -h)
 		# Save the current LaMP session ID
-		lamp_session_id=decoded_data[1];
+		lamp_session_id=decoded_data[1]
 
-		late_fields=[];
+		late_fields=[]
 		# Gather the field names from the first TCP packet (i.e. 'LaTeINIT')
 		for field in decoded_data[2].split("=")[1].split(";"):
-			late_fields.append(field);
+			late_fields.append(field)
 		
-		print("Current ID:",lamp_session_id);
+		print("Current ID:",lamp_session_id)
 
 		# Start waiting for and receiving UDP packets, in a separate thread (point (a.1) ./LaTe -h)
 		# udp_rx_loop() will take into account the points (a.2) and (a.3) in ./LaTe -h
-		t=Thread(target=udp_rx_loop,args=(udp_descriptor,lamp_session_id,late_fields));
+		t=Thread(target=udp_rx_loop,args=(udp_descriptor,lamp_session_id,late_fields))
 		try:
-			t.start();
+			t.start()
 		except:
-			print("Error: unable to start UDP reception thread.");
-			break;
+			print("Error: unable to start UDP reception thread.")
+			break
 
 		# Wait for the last TCP packet
-		data=tcp_descriptor_client.recv(2048);
+		data=tcp_descriptor_client.recv(2048)
 
 		# If the TCP client disconnects before receiving the 'LaTeEND' packet, print an error and terminate the current connection
 		if len(data)==0:
-			print("Error: LaTe was disconnected before LaTeEND could be received.");
-			terminate_udp_rx_loop(udp_descriptor,opts,lamp_session_id);
-			t.join();
-			tcp_descriptor_client.close();
-			continue;
+			print("Error: LaTe was disconnected before LaTeEND could be received.")
+			terminate_udp_rx_loop(udp_descriptor,opts,lamp_session_id)
+			t.join()
+			tcp_descriptor_client.close()
+			continue
 		
-		decoded_data=data.decode("utf-8").split(",");
+		decoded_data=data.decode("utf-8").split(",")
 
 		# Check if this packet is a 'LaTeEND' one
 		if decoded_data[0] != "LaTeEND":
 			print("Expected a LaTeEND packet, but received",decoded_data[0])
-			print("No final report data can be obtained.");
+			print("No final report data can be obtained.")
 		else:
 			# When 'LateEND' is received and if it has the right ID, save all the report data contained inside, processing it
 			# (point (a.4) in ./LaTe -h)
-			print("Received a",decoded_data[0],"packet");
+			print("Received a",decoded_data[0],"packet")
 
 			if decoded_data[1] != lamp_session_id:
-				print("Warning: data ignored. Expected test ID",lamp_session_id,"but received",decoded_data[1]);
+				print("Warning: data ignored. Expected test ID",lamp_session_id,"but received",decoded_data[1])
 			else:
 				# If 'LaTeEND' has the third field set to 'srvtermination', it should be considered just a way to gracefully
 				# terminate the current connection after a unidirectional test, without carrying any particular final report data
@@ -204,11 +204,11 @@ def main (argv):
 					final_test_data_operations(decoded_data)
 
 		# Stop waiting for new UDP packets (point (a.4) in ./LaTe -h)
-		terminate_udp_rx_loop(udp_descriptor,opts,lamp_session_id);
-		t.join();
+		terminate_udp_rx_loop(udp_descriptor,opts,lamp_session_id)
+		t.join()
 
 		# Close any TCP socket which was left open before accepting a new connection on the same port (point (2) in ./LaTe -h)
-		tcp_descriptor_client.close();
+		tcp_descriptor_client.close()
 
 
 
@@ -217,29 +217,29 @@ def main (argv):
 # ----------------------------------------------------------------------------------------------------------------------------------
 # This function is used to print the application usage and then exit with an error code equal to 'exit_value'
 def print_short_info_and_exit(exit_value):
-	print("Usage: python3 " + os.path.basename(__file__) + " -a <address to bind the socket to>:<port>");
-	sys.exit(exit_value);
+	print("Usage: python3 " + os.path.basename(__file__) + " -a <address to bind the socket to>:<port>")
+	sys.exit(exit_value)
 
 # Function used to parse the command line options, supporting -h (to print a short help) and -a (or --bindaddress - to specify <IP>:<port>)
 def parse_options(argv):
 	# Create a new 'struct_options' (see the @dataclass at the beginning of this file)
-	options=struct_options();
+	options=struct_options()
 
 	# Command line options parser
 	try:
-		opts,args=getopt.getopt(argv,"ha:e:",["bindaddress=",]);
+		opts,args=getopt.getopt(argv,"ha:e:",["bindaddress=",])
 	except getopt.GetoptError as arg_error:
-		print(str(arg_error));
-		print_short_info_and_exit(1);
+		print(str(arg_error))
+		print_short_info_and_exit(1)
 
 	for opt,arg in opts:
 		if opt == '-h':
 			print_short_info_and_exit(0);
 		elif opt in ("-a", "--lateaddress"):
 			# Parse the <IP>:<port> string
-			split_arg=arg.split(":");
-			options.IPv4=split_arg[0];
-			options.port=int(split_arg[1]);
+			split_arg=arg.split(":")
+			options.IPv4=split_arg[0]
+			options.port=int(split_arg[1])
 
 			# Check if the specified IP and port are correct
 			try:
