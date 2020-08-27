c46d0675770a9e53a9ff193c50e337384c6a6cd6
Francesco Raviglione <francescorav.es483@gmail.com>
2020-08-27 11:31:51 +0200

    New development branch for v0.1.7-beta: set new version number

--- include/version.h
+++ include/version.h
@@ -2,11 +2,11 @@
 #define LATENCYTEST_VERSION_H_INCLUDED
 
 #if !AMQP_1_0_ENABLED
-#define VERSION "0.1.6-beta"
+#define VERSION "0.1.7-beta-development"
 #else
-#define VERSION "0.1.6-beta - AMQP 1.0 support"
+#define VERSION "0.1.7-beta-development - AMQP 1.0 support"
 #endif
-#define DATE "20200826e"
+#define DATE "20200827a"
 #define PROG_NAME_LONG "LaTe tool"
 #define PROG_NAME_SHORT "LaTe"
 #define GITHUB_LINK "https://github.com/francescoraves483/LaMP_LaTe"
