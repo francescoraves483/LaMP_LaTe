francescoraves483/LaMP_LaTe

commit 025c2121de9851c240a2fe53e72125ad0ec49f72

Introducing the --log-init-failures option
This commit introduces a new option, to be used in conjunction with
-f, to log to the CSV file also the tests which failed duiring the
INIT procedure, when no connection can be established between the
client and the server.

include/version.h
@@ -6,7 +6,7 @@
 #else
 #define VERSION "0.1.7-beta-development - AMQP 1.0 support"
 #endif
-#define DATE "20201105a"
+#define DATE "20201123a"
 #define PROG_NAME_LONG "LaTe tool"
 #define PROG_NAME_SHORT "LaTe"
 #define GITHUB_LINK "https://github.com/francescoraves483/LaMP_LaTe"

src/udp_client.c
@@ -1105,12 +1105,12 @@ unsigned int runUDPclient(struct lampsock_data sData, struct options *opts) {
 		// Directly return only if a timeout did not occur, as, in case of timeout, we should still print the report.
 		// If we exit now, losing the last packet (ENDREPLY or ENDREPLY_TLESS), which causes a timeout in the rx loop,
 		// may mean losing the whole test, which is not desiderable
-		if(t_rx_error!=ERR_TIMEOUT && t_rx_error!=ERR_REPORT_TIMEOUT) {
+		if(t_rx_error!=ERR_TIMEOUT && t_rx_error!=ERR_REPORT_TIMEOUT && opts->log_init_failures==0) {
 			return 1;
 		}
 	}
 
-	if(t_rx_error!=ERR_REPORT_TIMEOUT) {
+	if(t_rx_error!=ERR_REPORT_TIMEOUT && t_rx_error!=NO_ERR) {
 		/* Ok, the mode_ub==UNSET_UB case is not managed, but it should never happen to reach this point
 		with an unset mode... at least not without getting errors or a chaotic thread behaviour! But it should not happen anyways. */
 		fprintf(stdout,opts->mode_ub==PINGLIKE?"Ping-like ":"Unidirectional " "statistics:\n");

src/udp_client_raw.c
@@ -1239,13 +1239,13 @@ unsigned int runUDPclient_raw(struct lampsock_data sData, macaddr_t srcMAC, stru
 		// Directly return only if a timeout did not occur, as, in case of timeout, we should still print the report.
 		// If we exit now, losing the last packet (ENDREPLY or ENDREPLY_TLESS), which causes a timeout in the rx loop,
 		// may mean losing the whole test, which is not desiderable
-		if(t_rx_error!=ERR_TIMEOUT && t_rx_error!=ERR_REPORT_TIMEOUT) {
+		if(t_rx_error!=ERR_TIMEOUT && t_rx_error!=ERR_REPORT_TIMEOUT && opts->log_init_failures==0) {
 			return 1;
 		}
 		return 1;
 	}
 
-	if(t_rx_error!=ERR_REPORT_TIMEOUT) {
+	if(t_rx_error!=ERR_REPORT_TIMEOUT && t_rx_error!=NO_ERR) {
 		/* Ok, the mode_ub==UNSET_UB case is not managed, but it should never happen to reach this point
 		with an unset mode... at least not without getting errors or a chaotic thread behaviour! But it should not happen anyways. */
 		fprintf(stdout,opts->mode_ub==PINGLIKE?"Ping-like ":"Unidirectional " "statistics:\n");

include/options.h
@@ -196,6 +196,7 @@ struct options {
 	char *Wfilename; // Filename for the -W mode
 	uint8_t printAfter; // Server only. =0 if the server should print that a packet was received before sending the reply, =1 to print after sending the reply (default: 0)
 	uint8_t initial_timeout_server;
+	uint8_t log_init_failures;
 
 	union {
 		struct in_addr destIPaddr;

src/options.c
@@ -72,12 +72,14 @@
 #define LONGOPT_U "unidir"
 
 #define LONGOPT_initial_timeout "initial-timeout"
+#define LONGOPT_log_init_failures "log-init-failures"
 
 #define LONGOPT_t_client "interval"
 #define LONGOPT_t_server "server-timeout"
 #define LONGOPT_t_client_val 256
 #define LONGOPT_t_server_val 257
 #define LONGOPT_initial_timeout_server_val 258
+#define LONGOPT_log_init_failures_client_val 259
 
 #define LONGOPT_STR_CONSTRUCTOR(LONGOPT_STR) "  --"LONGOPT_STR"\n"
 
@@ -95,6 +97,7 @@ static const struct option late_long_opts[]={
 	{LONGOPT_t_client,	required_argument, 	NULL, LONGOPT_t_client_val},
 	{LONGOPT_t_server,	required_argument, 	NULL, LONGOPT_t_server_val},
 	{LONGOPT_initial_timeout, no_argument, NULL, LONGOPT_initial_timeout_server_val},
+	{LONGOPT_log_init_failures, no_argument, NULL, LONGOPT_log_init_failures_client_val},
 	{LONGOPT_z,			required_argument,	NULL, 'z'},
 	{LONGOPT_A,			required_argument,	NULL, 'A'},
 	{LONGOPT_C,			required_argument,	NULL, 'C'},
@@ -413,6 +416,12 @@ static const struct option late_long_opts[]={
 	"  --"LONGOPT_initial_timeout": make the server terminate after the timeout specified with -t, even if no client\n" \
 	"\t   attempted a connection.\n"
 
+#define OPT_log_init_failures_client \
+	"  --"LONGOPT_log_init_failures": enables logging of empty lines to the CSV file specified with -f when failures\n" \
+	"\t   occur during the INIT procedure. The normal behaviour, when no connection can be established between client\n" \
+	"\t   and server, is to leave the CSV file untouched, without adding any line related to a failed test.\n" \
+	"\t   This option is client-only and can be selected only together with -f or --"LONGOPT_f".\n"
+
 static const char *latencyTypes[]={"Unknown","User-to-user","KRT","Software (kernel) timestamps","Hardware timestamps"};
 
 // Safer implementation of strchr for the -w option, reading up to the maximum expected number of characters in -w (i.e. MAX_w_STRING_SIZE)
@@ -572,6 +581,7 @@ static void print_long_info(void) {
 			OPT_R_client	
 			OPT_T_client
 			OPT_V_both
+			OPT_log_init_failures_client
 
 			// File options
 			OPT_f_client
@@ -707,6 +717,7 @@ void options_initialize(struct options *options) {
 	options->duration_interval=0;
 	options->payloadlen=0;
 	options->initial_timeout_server=0;
+	options->log_init_failures=0;
 
 	// Initial UP is set to 'UINT8_MAX', as it should not be a valid value
 	// When this value is detected by the application, no setsockopt is called
@@ -1480,6 +1491,10 @@ unsigned int parse_options(int argc, char **argv, struct options *options) {
 				options->initial_timeout_server=1;
 				break;
 
+			case LONGOPT_log_init_failures_client_val:
+				options->log_init_failures=1;
+				break;
+
 			default:
 				print_short_info_err(options);
 
@@ -1563,6 +1578,10 @@ unsigned int parse_options(int argc, char **argv, struct options *options) {
 			fprintf(stderr,"Error: --interval is a client only option.\n");
 			print_short_info_err(options);
 		}
+		if(options->log_init_failures==1) {
+			fprintf(stderr,"Error: --"LONGOPT_log_init_failures" is a client only option.\n");
+			print_short_info_err(options);
+		}
 	} else if(options->mode_cs==LOOPBACK_CLIENT) {
 		if(eI_flag==1) {
 			fprintf(stderr,"Error: -I/-e are not supported when using loopback interfaces, as only one interface is used.\n");
@@ -1628,6 +1647,10 @@ unsigned int parse_options(int argc, char **argv, struct options *options) {
 			fprintf(stderr,"Error: --interval is a client only option.\n");
 			print_short_info_err(options);
 		}
+		if(options->log_init_failures==1) {
+			fprintf(stderr,"Error: --"LONGOPT_log_init_failures" is a client only option.\n");
+			print_short_info_err(options);
+		}
 	}
 
 	#if AMQP_1_0_ENABLED
@@ -1691,6 +1714,11 @@ unsigned int parse_options(int argc, char **argv, struct options *options) {
 		}
 	}
 	#endif
+
+	if(options->log_init_failures==1 && options->filename==NULL) {
+			fprintf(stderr,"Error: --"LONGOPT_log_init_failures" can only be specified together with -f or --"LONGOPT_f".\n");
+			print_short_info_err(options);
+	}
 
 	// -i and -z cannot be specified together
 	if(options->seconds_to_end!=-1 && options->duration_interval!=0) {

commit e51e596e38cf90079a1ae251258f9ca39cc4a4ac

Fix bug which was preventing the final report from being printed
This commit fixes a bug, introduced in commit
025c2121de9851c240a2fe53e72125ad0ec49f72
which was causing the final report to be never printed when a test
was correctly terminated.

include/version.h
@@ -6,7 +6,7 @@
 #else
 #define VERSION "0.1.7-beta-development - AMQP 1.0 support"
 #endif
-#define DATE "20201218a"
+#define DATE "20200203c"
 #define PROG_NAME_LONG "LaTe tool"
 #define PROG_NAME_SHORT "LaTe"
 #define GITHUB_LINK "https://github.com/francescoraves483/LaMP_LaTe"

src/udp_client.c
@@ -1110,13 +1110,15 @@ unsigned int runUDPclient(struct lampsock_data sData, struct options *opts) {
 		}
 	}
 
-	if(t_rx_error!=ERR_REPORT_TIMEOUT && t_rx_error!=NO_ERR) {
-		/* Ok, the mode_ub==UNSET_UB case is not managed, but it should never happen to reach this point
-		with an unset mode... at least not without getting errors or a chaotic thread behaviour! But it should not happen anyways. */
-		fprintf(stdout,opts->mode_ub==PINGLIKE?"Ping-like ":"Unidirectional " "statistics:\n");
-		// Print the statistics, if no error, before returning
-		reportStructureFinalize(&reportData);
-		printStats(&reportData,stdout,opts->confidenceIntervalMask);
+	if(opts->log_init_failures==0 || (opts->log_init_failures==1 && t_rx_error!=ERR_TIMEOUT_ACK)) {
+		if(t_rx_error!=ERR_REPORT_TIMEOUT) {
+			/* Ok, the mode_ub==UNSET_UB case is not managed, but it should never happen to reach this point
+			with an unset mode... at least not without getting errors or a chaotic thread behaviour! But it should not happen anyways. */
+			fprintf(stdout,opts->mode_ub==PINGLIKE?"Ping-like ":"Unidirectional " "statistics:\n");
+			// Print the statistics, if no error, before returning
+			reportStructureFinalize(&reportData);
+			printStats(&reportData,stdout,opts->confidenceIntervalMask);
+		}
 	}
 
 	if(opts->filename!=NULL) {

src/udp_client_raw.c
@@ -1242,16 +1242,17 @@ unsigned int runUDPclient_raw(struct lampsock_data sData, macaddr_t srcMAC, stru
 		if(t_rx_error!=ERR_TIMEOUT && t_rx_error!=ERR_REPORT_TIMEOUT && opts->log_init_failures==0) {
 			return 1;
 		}
-		return 1;
 	}
 
-	if(t_rx_error!=ERR_REPORT_TIMEOUT && t_rx_error!=NO_ERR) {
-		/* Ok, the mode_ub==UNSET_UB case is not managed, but it should never happen to reach this point
-		with an unset mode... at least not without getting errors or a chaotic thread behaviour! But it should not happen anyways. */
-		fprintf(stdout,opts->mode_ub==PINGLIKE?"Ping-like ":"Unidirectional " "statistics:\n");
-		// Print the statistics, if no error, before returning
-		reportStructureFinalize(&reportData);
-		printStats(&reportData,stdout,opts->confidenceIntervalMask);
+	if(opts->log_init_failures==0 || (opts->log_init_failures==1 && t_rx_error!=ERR_TIMEOUT_ACK)) {
+		if(t_rx_error!=ERR_REPORT_TIMEOUT) {
+			/* Ok, the mode_ub==UNSET_UB case is not managed, but it should never happen to reach this point
+			with an unset mode... at least not without getting errors or a chaotic thread behaviour! But it should not happen anyways. */
+			fprintf(stdout,opts->mode_ub==PINGLIKE?"Ping-like ":"Unidirectional " "statistics:\n");
+			// Print the statistics, if no error, before returning
+			reportStructureFinalize(&reportData);
+			printStats(&reportData,stdout,opts->confidenceIntervalMask);
+		}
 	}
 
 	if(opts->filename!=NULL) {